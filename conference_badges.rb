4c9d659e09bccdcf7a76295215e77fcc207235b6
Zannatul Naeem <mollahnaeem@gmail.com>
2020-07-29 22:52:45 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,21 @@
-# Write your code here.
+attendee= ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def badge_maker(name)
+return "Hello, my name is #{name}."
+end
+
+
+
+def batch_badge_creator(attendee)
+  badges= []
+  attendee.each do |names| 
+   badges.push("Hello, my name is #{names}.")
+  end
+  return badges
+end
+
+def assign_rooms(attendee)
+  room_assignments= []
+  attendee.each do |names|
+    room_assignments.push("Hello, #{names}! You'll be assigned to room ")
+end 
